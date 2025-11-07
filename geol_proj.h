a3baac02f9ee2d361f4df86aa3628acb5f776498
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-11-07 17:15:47 +0100

    feat: adds eceftowgs84

--- geol_proj.h
+++ geol_proj.h
@@ -11,15 +11,26 @@ void WGS84toECEF_rad(
 	double  lat, double  lon, double  h
 );
 
+void ECEFtoWGS84_deg(
+	double* lat, double* lon, double* h,
+	double  x,   double  y,   double  z
+);
+
+void ECEFtoWGS84_rad(
+	double* lat, double* lon, double* h,
+	double  x,   double  y,   double  z
+);
+
 #ifdef GEOL_PROJ_IMPLEMENTATION
 
 #include <math.h>
 
-#define _geol_pi 3.1415926535898
-#define _geol_a  6378137.0
-#define _geol_b  6356752.3142
-#define _geol_f  0.00335281066475
-#define _geol_E  0.00669437999014
+#define _geol_pi 3.1415926535898  // pi
+#define _geol_a  6378137.0        // constant - earth major axis
+#define _geol_b  6356752.3142     // constant - earth minor axis
+#define _geol_f  0.00335281066475 // (a - b) / a - ellipsoid flatness
+#define _geol_E  0.00669437999014 // f * (2 - f) - square of eccentricity
+#define _geol_Es 0.00673949674228 // E / (1 - E)
 
 #define _geol_degtorad(deg) ((deg) * _geol_pi / 180.0)
 #define _geol_radtodeg(rad) ((rad) * 180.0 / _geol_pi)
@@ -45,6 +56,35 @@ void WGS84toECEF_rad(
 	*x = (h + N) * cos(lat) * cos(lon);
 	*y = (h + N) * cos(lat) * sin(lon);
 	*z = (h + N * (1 - _geol_E)) * sin(lat);
+}
+
+void ECEFtoWGS84_deg(
+	double* lat, double* lon, double* h,
+	double  x,   double  y,   double  z
+) {
+	ECEFtoWGS84_rad(lat, lon, h, x, y, z);
+	*lat = _geol_radtodeg(*lat);
+	*lon = _geol_radtodeg(*lon);
+}
+
+void ECEFtoWGS84_rad(
+	double* lat, double* lon, double* h,
+	double  x,   double  y,   double  z
+) {
+	const double p     = sqrt(x * x + y * y);
+	const double q     = atan2((z * _geol_a), (p * _geol_b));
+	const double qs    = sin(q);
+	const double qc    = cos(q);
+	const double qs3   = qs * qs * qs;
+	const double qc3   = qc * qc * qc;
+	const double phi_a = z + _geol_Es * _geol_b * qs3;
+	const double phi_b = p - _geol_E * _geol_a * qc3;
+	const double phi   = atan2(phi_a, phi_b);
+	const double lam   = atan2(y, x);
+	const double v     = _geol_a / sqrt(1 - _geol_E * sin(phi) * sin(phi));
+	*lat = phi;
+	*lon = lam;
+	*h = (p / cos(phi)) - v;
 }
 
 #endif
--- test/main.c
+++ test/main.c
@@ -43,8 +43,8 @@ void test_WGS84toECEF() {
 		{.x = 4517591, .y = 0, .z = 4487348},
 		{.x = 0, .y = 0, .z = 6356752},
 		{.x = -6378137, .y = 0, .z = 0},
-		{.x =   0, .y = -6378137, .z = 0},
-		{.x =   0, .y = 6378137, .z = 0},
+		{.x = 0, .y = -6378137, .z = 0},
+		{.x = 0, .y = 6378137, .z = 0},
 		{.x = -6378137, .y = 0, .z = 0},
 		{.x = 3194419, .y = -3194419, .z = -4487348},
 		{.x = 3194419, .y = 3194419, .z = -4487348},
@@ -65,6 +65,30 @@ void test_WGS84toECEF() {
 	}
 }
 
+void test_ECEFtoWGS84() {
+	printf("----ECEFtoWGS84----\n");
+	const size_t count = 3;
+	const vec3_t tests[] = {
+		{.x = 6378137, .y = 0, .z = 0},
+		{.x = 0, .y = 6378137, .z = 0},
+		{.x = 6378137, .y = 0, .z = 1000000},
+	};
+	const vec3_t expected[] = {
+		{.x = 0, .y = 0, .z = 0},
+		{.x = 0, .y = 90, .z = 0},
+		{.x = 9, .y = 0, .z = 78432},
+	};
+	for (size_t i = 0; i < count; i++) {
+		printf("%zu...", i);
+		fflush(stdout);
+		vec3_t a;
+		ECEFtoWGS84_deg(&a.x, &a.y, &a.z, tests[i].x, tests[i].y, tests[i].z);
+		assert(cmp(a, expected[i]));
+		printf("ok\n");
+	}
+}
+
 int main(int argc, char** argv) {
 	test_WGS84toECEF();
+	test_ECEFtoWGS84();
 }
